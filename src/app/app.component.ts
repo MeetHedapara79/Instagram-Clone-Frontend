4e5683105c87e2b3a02bbe32b63310c3246057dd
Meet Hedapara <meet.hedapara@pc0677.radixweb.local>
2025-07-18 17:44:50 +0530

    fix:build issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,8 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'instagram';
+  title:string = 'instagram';
+  ngOnInit(): void {
+    console.log("✅ Frontend App is running successfully 🚀");
+  }
 }
