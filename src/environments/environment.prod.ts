a2244776796e6f4db12f4b518603b9ef035a5d17
Meet Hedapara <meet.hedapara@pc0677.radixweb.local>
2025-07-18 17:07:10 +0530

    update environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://instagram-clone-backend-production-ad22.up.railway.app/api/v1/',
+    socketUrl: 'http://instagram-clone-backend-production-ad22.up.railway.app',
+    appUrl: 'http://instagram-clone-frontend-production.up.railway.app',
+};
